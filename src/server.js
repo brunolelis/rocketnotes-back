8f367e03a6d941314976f614d752471eef229c2b
Bruno Lelis <brunolelis.dev@gmail.com>
2024-10-10 20:48:52 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ const AppError = require("./utils/AppError")
 const migrationsRun = require("./database/sqlite/migrations")
 const uploadConfig = require("../src/config/upload")
 
+const cors = require("cors")
 const express = require('express')
 const routes = require("./routes")
 
 migrationsRun()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
