b14e3a56f17bc4c4ced817cef02f8b451b5a6304
Bruno Lelis <brunolelis.dev@gmail.com>
2024-10-31 20:27:19 -0300

    UserCreateServiceSpec-beforeEach

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -4,6 +4,14 @@ const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory')
 const AppError = require("../utils/AppError")
 
 describe("UserCreateService", () => {
+  let userRepositoryInMemory = null
+  let userCreateService = null
+
+  beforeEach(() => {
+    userRepositoryInMemory = new UserRepositoryInMemory()
+    userCreateService = new UserCreateService(userRepositoryInMemory)
+  })
+
   it("user should be created", async () => {
     const user = {
       name: "User Test",
@@ -11,8 +19,6 @@ describe("UserCreateService", () => {
       password: "123"
     };
 
-    const userRepositoryInMemory = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepositoryInMemory)
     const userCreated = await userCreateService.execute(user)
 
     console.log(userCreated)
@@ -31,9 +37,6 @@ describe("UserCreateService", () => {
       email: "user@test.com",
       password: "456"
     }
-
-    const userRepositoryInMemory = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepositoryInMemory)
 
     await userCreateService.execute(user1)
     await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Email already registered!"))
